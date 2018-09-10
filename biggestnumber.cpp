1c5f72b04f953307d5aa1308e18457db29c39f5a
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-09-10 16:17:11 -0700

    version of biggest number that only takes and stores ints without comparison

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,18 +1,21 @@
-//Author:
+//Author: Robert DeValentine
 #include<iostream>
 
 int main()
 {
   //declare variables here
-
+    int age;
+    int age2;
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
+  std::cin>>age;
 
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
+  std::cin>>age2;
 
   //determine if first or second is bigger and store that in a third variable
-
+  
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
