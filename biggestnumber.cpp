2acf002ae5afbd3ef6f26224a9577c7c96abf5b8
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-09-10 17:04:56 -0700

    version of BiggestNumber that properly campares and states biggest number.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,19 +6,39 @@ int main()
   //declare variables here
     int age;
     int age2;
+    int biggest;
+
   std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
+          
   std::cin>>age;
+  //get first input using std::cin>>
 
   std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+          
   std::cin>>age2;
+  //get second input using std::cin>> again with a second variable
 
   //determine if first or second is bigger and store that in a third variable
-  
+  if(age > age2)
+    {
+        biggest = age;
+
+    }
+  else if(age2 >= age)
+    {
+        biggest = age2;
+
+    }
+  else
+    {
+        std::cout<<"Use numerals please ignore the following                    ";
+
+    }
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
+  std::cout<<biggest;
+  
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
